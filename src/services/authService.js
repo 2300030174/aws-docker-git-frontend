7ceed5394813a63d6d14a959186e873351653c2b
Duggempudi ThanujReddy <2300030174@kluniversity.in>
2025-09-15 10:23:27 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-18-234-236-147.compute-1.amazonaws.com/auth";
+const API_URL = "http://50.17.61.136:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
@@ -15,3 +15,4 @@ export const signup = async (username, email, password) => {
 export const logout = () => {
   localStorage.removeItem("token");
 };
+
