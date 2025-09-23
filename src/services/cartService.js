d3b3e1e7eef9077b4a2e1ad186ca1f4549d9a072
Duggempudi ThanujReddy <2300030174@kluniversity.in>
2025-09-23 08:06:00 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://3.89.242.54:8081/api/cart";
+const API_URL = "http://34.228.43.84:8081/api/cart";
 
-const API_BASE = "http://3.89.242.54:8081/api/cart";
+const API_BASE = "http://34.228.43.84:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
@@ -29,6 +29,7 @@ export const removeCartItem = async (cartItemId) => {
         console.error("Error removing item from cart", error);
     }
 };
+
 
 
 
